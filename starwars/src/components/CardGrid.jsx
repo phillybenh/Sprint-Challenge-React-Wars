bc9d095648019d66efb5169afe74bde617a87272
Ben Haus <ben@little-haus.com>
2020-03-20 13:02:16 -0400

    committing MVP

--- starwars/src/components/CardGrid.jsx
+++ starwars/src/components/CardGrid.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import CharacterCard from "./CharacterCard"
+import styled from "styled-components";
 
+//style
+const CardContainer = styled.div`
+margin: 10px;
+width: 100%;
+padding: 4%;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-around;
+`;
+
+//function
 export default function CardGrid(props) {
 //   console.log(props.characters);
 
 
 return (
-  <div>
+  <CardContainer>
     {props.characters.map((item, index) => {
-      return (
-        <CharacterCard
-          key={index}
-          item={item}
-        />
-      );
+      return <CharacterCard key={index} item={item} />;
     })}
-  </div>
+  </CardContainer>
 );
 
 
--- starwars/src/components/CharacterCard.jsx
+++ starwars/src/components/CharacterCard.jsx
@@ -1,19 +1,45 @@
 import React from "react";
+import styled from "styled-components"
 
+//Styles
+const Card = styled.section`
+  background: black;
+  color: yellow;
+  width: 225px;
+  max-height: 300px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  // align-items: flex-start;
+  text-align: left;
+  padding: 10px;
+  margin: 10px;
+`;
+const List = styled.ul`
+padding:10px;
+margin: 0 20px;
+`;
+const Para = styled.p`
+margin-bottom: 0px;
+`;
+
+
+//Function
 export default function CharacterCard(props) {
   // console.log(props.item)
   const data = props.item;
   return (
-    <div>
+    <Card>
       <h2>{data.name}</h2>
-      <p>Character Data:</p>
-      <ul>
+      <Para>Character Data:</Para>
+      <List>
         <li>Birth Year: {data.birth_year}</li>
         <li>Gender: {data.gender}</li>
         <li>Eye Color: {data.eye_color}</li>
-        <li>Hair Color: {data.birth_year}</li>
+        <li>Hair Color: {data.hair_color}</li>
         <li>Height: {data.height}cm</li>
-      </ul>
-    </div>
+      </List>
+    </Card>
   );
 }
